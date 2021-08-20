4f48c883afe9bfb6563cb89bcd1f6f43a756047f
Ankit <ankitmaurya0225@gmail.com>
2021-08-20 18:00:34 +0530

    added new packages

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
  const express = require('express')
-
+ const fareprice = require('./fare')
  const app = express()
 
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.use(express.static(__dirname + '/public\ static'))
+
+app.post('/calculate_fare', (req, res) => {
+    let km = parseFloat(req.body.km)
+    let min = parseInt(req.body.min)
+
+    let fare = fareprice.calculate_Fare(km, min)
+
+    res.send({fare: fare})
+})
+
+app.get('/rate',( req,res) => {
+    res.send(fareprice.rate)
+})
 
  PORT = process.env.PORT || 8080
 
